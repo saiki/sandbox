f1fa3735a8decf9b90ec4fc13f0237c517beaef0
斉木亜希雄 <akio@mba2013.local>
2017-09-01 06:06:29 +0900

    add vuetify sample.

---
+++ vuetify-sample-app/src/pages/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		{{ message }}
+    <router-link to="/hello">Go to Hello</router-link>
+	</div>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component'
+
+@Component
+export default class Home extends Vue {
+	message = 'Hello,World!'
+}
+
+</script>
