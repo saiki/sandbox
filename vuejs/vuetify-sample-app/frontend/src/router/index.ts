1023c6316f7ac7fcc6c1710e4749758294415eec
斉木亜希雄 <akio@mba2013.local>
2017-09-05 22:30:48 +0900

    Type指定を追加

--- vuejs/vuetify-sample-app/frontend/src/router/index.ts
+++ vuejs/vuetify-sample-app/frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import Router, { RouteConfig } from 'vue-router'
+
 import Hello from '@/components/Hello.vue'
 import Home from '@/pages/Home.vue'
 
@@ -11,11 +12,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
-    },
+    } as RouteConfig,
     {
       path: '/hello',
       name: 'Hello',
       component: Hello
-    }
+    } as RouteConfig
   ]
 })
