d6f54bafd64692983d9591cd71d8ad7a774f5457
斉木亜希雄 <akio@mba2013.local>
2017-09-10 23:36:12 +0900

    add AppHeader

---
+++ vuejs/vuetify-sample-app/frontend/src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-toolbar>
+      <v-toolbar-side-icon @click.stop="onClickSideIcon"></v-toolbar-side-icon>
+      <v-btn icon @click.stop="onClickResize" v-show="drawer">
+        <v-icon v-if="mini">chevron_right</v-icon>
+        <v-icon v-else>chevron_left</v-icon>
+      </v-btn>
+    </v-toolbar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+    props: {
+        "drawer": Boolean,
+        "mini": Boolean
+    }
+})
+export default class AppHeader extends Vue {
+    onClickSideIcon() {
+        this.$emit("drawer")
+    }
+    onClickResize() {
+        this.$emit("mini")
+    }
+}
+</script>
